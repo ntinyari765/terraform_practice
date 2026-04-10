569637ae0312284ef4d801a92de3559ea7a5a4a5
ntinyari765 <winjoyntinyari765@gmail.com>
2026-04-10 17:00:46 +0300

    Add integrated CI pipeline, Sentinel policies, and Day 22 infrastructure

---
+++ terraform_day22/modules/webserver-cluster/outputs.tf
@@ -0,0 +1,34 @@
+output "cluster_name" {
+  description = "Name of the cluster"
+  value       = var.cluster_name
+}
+
+output "instance_type" {
+  description = "Instance type being used"
+  value       = local.instance_type
+}
+
+output "min_size" {
+  description = "Minimum number of instances"
+  value       = local.min_size
+}
+
+output "max_size" {
+  description = "Maximum number of instances"
+  value       = local.max_size
+}
+
+output "environment" {
+  description = "Environment this cluster is deployed in"
+  value       = var.environment
+}
+
+output "alarm_arn" {
+  description = "ARN of the high CPU alarm (null if monitoring disabled)"
+  value       = local.enable_monitoring ? aws_cloudwatch_metric_alarm.high_cpu[0].arn : null
+}
+
+output "instance_ip" {
+  description = "Public IP of a running EC2 instance"
+  value       = length(data.aws_instances.running.public_ips) > 0 ? data.aws_instances.running.public_ips[0] : null
+}
